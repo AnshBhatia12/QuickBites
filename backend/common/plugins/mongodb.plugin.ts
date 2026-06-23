d00328dbf0989c35505a60bb80c801e238e5f322
anshbhatia <anshbhatia@gmail.com>
2026-06-23 17:08:18 +0530

    major updates

--- backend/common/plugins/mongodb.plugin.ts
+++ backend/common/plugins/mongodb.plugin.ts
@@ -6,6 +6,7 @@ export const mongoPlugin = new Elysia({ name: "mongo-plugin" }).onStart(
     const uri = process.env.MONGO_URI;
     if (!uri) throw new Error("MONGO_URI is not defined in .env");
     await mongoose.connect(uri);
+
     console.log("🍃 MongoDB connected");
   }
 );
