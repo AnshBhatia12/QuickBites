f82f22da1b8eb30653cc6cf0a02b516990f57b7e
anshbhatia <anshbhatia@gmail.com>
2026-06-23 17:09:22 +0530

    updaets

--- backend/common/plugins/mongodb.plugin.ts
+++ backend/common/plugins/mongodb.plugin.ts
@@ -5,6 +5,7 @@ export const mongoPlugin = new Elysia({ name: "mongo-plugin" }).onStart(
   async () => {
     const uri = process.env.MONGO_URI;
     if (!uri) throw new Error("MONGO_URI is not defined in .env");
+
     await mongoose.connect(uri);
 
     console.log("🍃 MongoDB connected");
